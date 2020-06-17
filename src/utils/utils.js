cfba003ab0967b69c71b4b60bc9fa70281ed509f
Felpi <felipebmagagnin@gmail.com>
2020-06-17 14:47:24 -0300

    add utils class

---
+++ src/utils/utils.js
@@ -0,0 +1,26 @@
+export function TimestapToDate(timestamp) {
+  var date = new Date(Number(timestamp) * 1000);
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
+
+  // Will display time in 10:30:23 format
+  var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+  return formattedTime
+}
+
+export function TimestapToNumber(timestamp) {
+  var date = new Date(Number(timestamp) * 1000);
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  var seconds = "0" + date.getSeconds();
+
+  // Will display time in 10:30:23 format
+  return date.getSeconds()
+}
